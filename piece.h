5fed5464daf285a5c74b10fc3a5b0c138b64f2e7
MarkusRieg <mrieg@uwaterloo.ca>
2025-07-19 20:13:53 -0400

    Made piece use vectors, finished subclass interfaces

--- piece.h
+++ piece.h
@@ -1,7 +1,11 @@
 #include <string>
+#include <vector>
 
-
+#ifndef BOARD_H
+#define BOARD_H
 class Board;
+#endif 
+
 struct Position;
 struct Colour;
 
@@ -34,8 +38,8 @@ class Piece {
         Colour getColour() const;
         Position getPosition() const;
 
-        virtual Position *validMoves() = 0;
-        bool verifyMoves(Position p);
+        virtual std::vector<Position> validMoves() = 0;
+        virtual bool verifyMove(Position p);
         bool move(Position p, PieceType promotion_type = PieceType::Queen);
 
 
--- queen.h
+++ queen.h
@@ -1,5 +1,12 @@
 #include "piece.h"
+#include <vector>
 
 class Queen : public Piece {
+    public:
+        Queen(Colour colour, Board *b);
 
+        Piece::PieceType getType() const override;
+
+        bool verifyMove(Piece::Position p) override;
+        std::vector<Position> validMoves() override;
 };
